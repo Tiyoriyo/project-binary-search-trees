1f92c9787a345e99c44f30fe89ca67d04be7b058
Vadim Kostrov <vmkvo15@outlook.com>
2023-06-26 17:26:28 +0400

    Add recursive insert function for BST

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import duplicateRemover from './duplicateRemover.js';
 import prettyPrint from './prettyPrint.js';
 
 const tree = (array) => {
-  let data = mergeSort(
+  let data = mergeSort( // Returns Sorted Array with duplicates removed.
     duplicateRemover(array),
   );
 
@@ -20,11 +20,49 @@ const tree = (array) => {
     root.setRight(buildTree(mid + 1, end));
     return root;
   }
+
+  function insertRec(root, value) {
+    if (root === null) { return node(value); }
+
+    if (value > root.root) {
+      root.setRight(insertRec(root.right, value));
+    } else if (value < root.root) {
+      root.setLeft(insertRec(root.left, value));
+    }
+    return root;
+  }
+
+  function insert(value) {
+    this.root = insertRec(this.root, value);
+  }
   return {
     root: buildTree(0, data.length - 1),
+    insert,
   };
 };
 
-const treeArray = tree([3, 3, 3, 1, 2, 3, 4]);
+const treeArray = tree([50, 23, 23, 10, 25, 34, 44]);
 // 2, 3, 4, 5, 6, 8, 10
 console.log(prettyPrint(treeArray.root));
+
+treeArray.insert(2);
+treeArray.insert(12);
+treeArray.insert(12);
+treeArray.insert(37);
+treeArray.insert(33);
+treeArray.insert(1);
+treeArray.insert(-12);
+console.log(prettyPrint(treeArray.root));
+
+// if (start > end) return null;
+// const mid = Math.floor((start + end) / 2);
+// const root = node(data[mid]);
+// root.setLeft(buildTree(start, mid - 1));
+// root.setRight(buildTree(mid + 1, end));
+// return root;
+
+// 3, 1, 2, 4
+// 1, 2, 3, 4   Mid = index 1;  mid - 1 = 0; mid + 1 = 2; Root = Node(2);
+// [1], /2/, [3, 4]
+// 1 Root = 0; buildTree(0, 0-1)
+// buildTreeRight(1, 0)
